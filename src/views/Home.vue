6b86dc971364975c2c401aeb3c9c981a27f4ca45
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-01 10:39:49 +0200

    "Added basic Home.vue template with Header component and 'Home' heading"

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Header />
+        <h1>Home</h1>
+        <!-- Add product listing here -->
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+</script>
