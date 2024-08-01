c768d55deb9d10aa3227ab8a3f988f9dd3f52df1
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-01 10:32:53 +0200

    "Modified main.js by adding router import and use in createApp"

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue' // import the root component App from a single-file component.
 import App from './App.vue'
+import router from './router';
 
-createApp(App).mount('#app') // The object we are passing into 'createApp' is in fact a component. 
+createApp(App).use(router).mount('#app') // The object we are passing into 'createApp' is in fact a component. 
