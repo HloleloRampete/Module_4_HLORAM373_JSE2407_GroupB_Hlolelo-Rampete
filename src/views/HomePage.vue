6acd6e646a99002a5e35021bd3674c5e66a96c20
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-01 13:45:30 +0200

    "Added product listing to HomePage.vue"

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,12 @@
     <div>
         <Navbar />
         <h1>Home</h1>
-        <!-- Add product listing here -->
+        <div v-if="products.length" class="grid grid-cols-4 gap-4">
+      <div v-for="product in products" :key="product.id" class="border p-4">
+        <h2 class="text-lg">{{ product.title }}</h2>
+        <p>{{ product.price }}</p>
+      </div>
+    </div>
     </div>
 </template>
 
