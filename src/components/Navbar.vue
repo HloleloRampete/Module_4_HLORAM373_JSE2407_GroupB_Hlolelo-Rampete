025ccc20e0bbe61216733afdfa88226c3b686242
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-01 13:04:38 +0200

    "Created basic Navbar component and exported file"

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="bg-gray-800 p-4 flex justify-between items-center">
+      <div class="text-white font-bold text-xl">
+        <router-link to="/">MyStore</router-link>
+      </div>
+      <div class="flex space-x-4">
+        <router-link to="/wishlist" class="text-white">Wishlist</router-link>
+        <router-link to="/cart" class="text-white">
+          <i class="fas fa-shopping-cart"></i>
+        </router-link>
+        <router-link to="/login" class="text-white">Login</router-link>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  };
+  </script>
+  
+ 
+  
