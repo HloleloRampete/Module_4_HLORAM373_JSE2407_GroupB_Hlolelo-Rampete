39a96a0799c3303531d48e2c10f87af4d3906492
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-01 13:13:28 +0200

    "Rename Home, Login, and Cart views to HomePage, LoginPage, and CartPage; delete old view files"

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '../views/HomePage.vue';
 import Wishlist from '../views/Wishlist.vue';
-import Login from '../views/Login.vue';
-import Cart from '../views/Cart.vue';
+import Login from '../views/LoginPage.vue';
+import Cart from '../views/CartPage.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
