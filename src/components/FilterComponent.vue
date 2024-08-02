3475bc1657829ae62f84724e68eb53a509af7c93
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-02 10:43:58 +0200

    "Created FilterComponent and SortComponent files and added categories dropdown and search input in FilterComponent"

---
+++ src/components/FilterComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center space-x-4">
+    <select
+      v-model="selectedCategory"
+      @change="filterProducts"
+      class="p-2 border rounded"
+    >
+      <option value="">All Categories</option>
+      <option v-for="category in categories" :key="category" :value="category">
+        {{ category }}
+      </option>
+    </select>
+    <input
+      type="text"
+      v-model="searchQuery"
+      @input="filterProducts"
+      placeholder="Search products..."
+      class="p-2 border rounded"
+    />
+  </div>
+</template>
