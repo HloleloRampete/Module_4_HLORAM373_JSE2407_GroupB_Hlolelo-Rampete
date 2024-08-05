533656c9b3f36ec38e53b2ec3cfb5a12b423273b
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-05 11:10:25 +0200

    "Abstracting the ProductList from the HomePage: create ProductList component and added product card details"

---
+++ src/components/ProductList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-if="products.length" class="grid grid-cols-4 gap-4">
+    <div v-for="product in products" :key="product.id" class="border p-4">
+      <h2 class="text-lg">{{ product.title }}</h2>
+      <p>{{ product.price }}</p>
+    </div>
+  </div>
+  <div v-else>
+    <p>No products found</p>
+  </div>
+</template>
