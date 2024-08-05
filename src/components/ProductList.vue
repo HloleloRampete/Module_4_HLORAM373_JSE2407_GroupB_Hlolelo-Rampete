8ae9ca49bb5e5f18c696a4021de1775b4f5d4c85
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-05 11:36:32 +0200

    "Modify your ProductList.vue to import and use the ProductCard component"

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="grid justify-center">
-    <div
-      class="lg:max-h-[130rem] max-w-xl mx-auto grid gap-4 grid-cols-1 lg:grid-cols-4 md:grid-cols-2 items-center lg:max-w-none my-4"
-    >
-      <div v-if="products.length" class="grid grid-cols-4 gap-4">
-        <div v-for="product in products" :key="product.id" class="border p-4">
-          <h2 class="text-lg">{{ product.title }}</h2>
-          <p>{{ product.price }}</p>
+    <div class="grid justify-center">
+      <div
+        class="lg:max-h-[130rem] max-w-xl mx-auto grid gap-4 grid-cols-1 lg:grid-cols-4 md:grid-cols-2 items-center lg:max-w-none my-4"
+      >
+        <div v-if="products.length" class="grid grid-cols-4 gap-4">
+          <ProductCard v-for="product in products" :key="product.id" :product="product" />
+        </div>
+        <div v-else>
+          <p>No products found</p>
         </div>
       </div>
-      <div v-else>
-        <p>No products found</p>
-      </div>
     </div>
-  </div>
-</template>
-
-<script>
-export default {
-  props: {
-    products: {
-      type: Array,
-      required: true,
+  </template>
+  
+  <script>
+  import ProductCard from "@/components/ProductCard.vue";
+  
+  export default {
+    components: {
+      ProductCard,
     },
-  },
-};
-</script>
+    props: {
+      products: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
