8e118594d354189e9a912bef82f5f51161d3ed1c
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-08-05 16:30:05 +0200

    "Created a new file named ProductDetail.vue in the components folder and added the necessary UI from the React project"

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-center">
+    <div class="grid justify-center">
       <div class="max-w-7xl w-full mx-auto grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-4">
         <ProductCard v-for="product in products" :key="product.id" :product="product" />
       </div>
